26c3f42ad76fddfdb5ed3a19e6fb08f33c86aae6
linruizhi1124@outlook.com <linruizhi1124@outlook.com>
2024-11-28 15:44:45 +0800

    数据结构：调整ringbuffer

--- ul_ringbuffer/ul_ringbuffer.h
+++ ul_ringbuffer/ul_ringbuffer.h
@@ -12,11 +12,12 @@
 #include "libarch.h"
 
 typedef struct {
-    uint16_t element_size;
-    uint16_t capacity;
-    void  *head; 
-    void  *tail;
-    void  *buff; //内存区,内存区长度为element_size*capacity
+    uint16_t element_size;  //元素大小
+    uint16_t capacity;  //元素容积量
+    uint32_t buff_size; //内存区长度，为element_size*capacity
+    void  *head; //头指针（读指针）
+    void  *tail; //尾指针（写指针）
+    void  *buff; //内存区
 }ul_ringbuffer_t;
 
 
@@ -26,8 +27,15 @@ typedef struct {
     void  *buff; //内存区,如果内存区为NULL,则会动态申请
 }ul_rb_attr_t;
 
-
-
-
+ul_ringbuffer_t* ul_ringbuffer_create( ul_rb_attr_t attr  );
+int ul_ringbuffer_init( ul_ringbuffer_t *rb, ul_rb_attr_t attr );
+int ul_ringbuffer_get_element_number( ul_ringbuffer_t *rb );
+int ul_ringbuffer_get_free_element_number( ul_ringbuffer_t *rb );
+int ul_ringbuffer_del_element( ul_ringbuffer_t *rb );
+int ul_ringbuffer_del_elements( ul_ringbuffer_t *rb, uint8_t count );
+int32_t ul_ringbuffer_add_element( ul_ringbuffer_t *rb, void* elem );
+int32_t ul_ringbuffer_add_elements( ul_ringbuffer_t *rb, void* elems, uint8_t count );
+int32_t ul_ringbuffer_get_element( ul_ringbuffer_t *rb, void* elem );
+int32_t ul_ringbuffer_get_elements( ul_ringbuffer_t *rb, void* elems, uint8_t count );
 #endif // !__UL_RINGBUFFER_H__
 
